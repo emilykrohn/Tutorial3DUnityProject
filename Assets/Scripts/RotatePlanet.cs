7ffbb62939969d43d76d96417010c5747bd077a1
Emily Krohn <emilykrohn2@gmail.com>
2024-03-16 15:39:50 -0700

    Fix CRLF to LF error and Learn about Start method
    CRLF to LF error was fixed after changing from Visual Studio 2019 to 2022. I also learned about how the start method runs when the game object the script is attached to is in the scene hierarchy and is active and enabled. Code in this method will run before the Update function code and it only runs one time.

--- Assets/Scripts/RotatePlanet.cs
+++ Assets/Scripts/RotatePlanet.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class RotatePlanet : MonoBehaviour
 {
     // Start is called before the first frame update
+    // Start will not run unless that game object it's attached to is in the scene hierarchy and is also active and enabled
     void Start()
     {
         
