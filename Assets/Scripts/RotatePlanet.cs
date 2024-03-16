985eff10961ea064fa16f442c900a1d2971f2ad0
Emily Krohn <emilykrohn2@gmail.com>
2024-03-16 16:28:26 -0700

    Learned how to get mouse input
    Prints out the position of the mouse at the time the left mouse button is clicked. Checks if the left mouse button is pressed every frame. If it was, then it stores the mouse position in pixel coordinates in the variable previousMousePosition and prints that out.

--- Assets/Scripts/RotatePlanet.cs
+++ Assets/Scripts/RotatePlanet.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class RotatePlanet : MonoBehaviour
 {
+    private Vector3 previousMousePosition; // Stores the mouse xyz position in a Vector3
+
     // Start is called before the first frame update
     // Start will not run unless that game object it's attached to is in the scene hierarchy and is also active and enabled
     void Start()
@@ -15,6 +17,11 @@ public class RotatePlanet : MonoBehaviour
     // Time between frames can vary so logic inside of this method should account for that for consistent behavior
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0)) // This will check if the mouse button has been pressed down regardless if it is held down or not
+        {
+            previousMousePosition = Input.mousePosition; // Get the current pixel coordinates that mouse is at in a Vector3
+                                                         // Then set that equal to the Vector3 previousMousePosition variable
+            Debug.Log("Previous Mouse Position: " + previousMousePosition); // Prints the mouse position at the time when the mouse was clicked
+        }
     }
 }
