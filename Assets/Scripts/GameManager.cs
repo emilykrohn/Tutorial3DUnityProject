8cf53483b62c5f1e30e7810cfc7c5f1f8d9c3746
Emily Krohn <emilykrohn2@gmail.com>
2024-03-17 20:34:54 -0700

    Create Game Manager Script
    Displays gold amount to the TextMeshProUGUI

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public int gold;
+    public TextMeshProUGUI goldDisplay;
+    public GameObject planet;
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        goldDisplay.text = gold.ToString();
     }
 }
