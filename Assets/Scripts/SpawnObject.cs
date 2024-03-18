a4294baf04f54b2d116104f3b4236dc8fb7825d9
Emily Krohn <emilykrohn2@gmail.com>
2024-03-17 20:42:42 -0700

    Create spawn function
    Function spawns in game object given in the parameter of the function and decreases the amount of gold by the cost of the object

--- Assets/Scripts/SpawnObject.cs
+++ Assets/Scripts/SpawnObject.cs
@@ -12,4 +12,13 @@ public class SpawnObject : MonoBehaviour
     {
         gm = FindAnyObjectByType<GameManager>();
     }
+
+    public void Spawn(GameObject objectToSpawn)
+    {
+        if(gm.gold >= cost) // if player has enough gold
+        {
+            Instantiate(objectToSpawn); // Spawn object in parameter
+            gm.gold -= cost; // Decrease the amount of gold the player has by how much the object costs
+        }
+    }
 }
