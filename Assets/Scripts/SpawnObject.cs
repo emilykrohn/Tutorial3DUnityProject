4b6aabf575598e6e6c7fb0101ef8335f4fa38551
Emily Krohn <emilykrohn2@gmail.com>
2024-03-17 20:38:46 -0700

    Create variable of type GameManager
    Find the game manager in the project and set the variable gm to it

--- Assets/Scripts/SpawnObject.cs
+++ Assets/Scripts/SpawnObject.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class SpawnObject : MonoBehaviour
 {
+    private GameManager gm;
+    public int cost;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        gm = FindAnyObjectByType<GameManager>();
     }
 }
